ffa1f1c4e2565903a46e1b9bc25af03c760e0198
PCF-Testing <m.shamraeva@gmail.com>
2015-06-30 04:49:00 -0700

    Added language selection drop-down and some log messages for debugging

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def allowed_file(filename):
 
 def _make_api_request(method='GET', path='/', data=None):
     url = app.config['api_url'] + path
+    print url
     headers = {
         'Content-Type': 'application/json',
         'X-Api-Username': session.get('username'),
@@ -53,15 +54,22 @@ def upload_to_s3(filename):
     bucket = app.config['S3_BUCKET']
     s3_conn = S3Connection(aws_id, aws_key)
     bucket = s3_conn.get_bucket(bucket)
+    print "Connected to S3 bucket"
     k = Key(bucket)
     k.key = os.path.join(app.config['S3_PATH'], os.path.basename(filename))
+    print "Created S3 key"
     k.set_contents_from_filename(filename)
+    print "Uploaded the video to S3"
     k.make_public()
+    print "Made the video public"
     return k
 
 @app.route('/')
 def index():
     if 'username' in session and 'api_key' in session:
+        resp = _make_api_request('get', '/languages')
+        language_dict = (json.loads(resp.content)).get('languages')
+        languages = sorted(language_dict.items(), key = lambda x: x[1])
         resp = _make_api_request('get', '/teams/?limit=100&offset=0')
         teams = (json.loads(resp.content)).get('objects')
         count = ((json.loads(resp.content)).get('meta')).get('total_count')
@@ -80,6 +88,7 @@ def index():
             'username': session['username'],
             'api_key': session['api_key'],
             'teams': teams,
+            'languages': languages,
         }
         return render_template('index.html', **ctx)
     else:
@@ -117,6 +126,7 @@ def upload():
                 'slug': project_slug
             }
             resp = _make_api_request('post', '/teams/'+team_slug+'/projects/',data=json.dumps(data))
+            print "Created project: "+ project_slug
             print resp.status_code
     for file in files:
         if file and allowed_file(file.filename):
@@ -129,13 +139,19 @@ def upload():
             sha.update(filename)
             #s3_filename = str(sha.hexdigest()) + '.mp4'
             s3_filename = time.strftime("%Y_%m_%d_%H_%M_%S_") + slugify(video_title) + '.mp4'
+            print "S3 filename:"
+            print s3_filename
             local_path = os.path.join(app.config['UPLOAD_DIR'], s3_filename)
+            print "Local path:"
+            print local_path
             # save local
             file.save(local_path)
+            print "Saved file to local path"
             # upload to s3
             key = upload_to_s3(local_path)
             # cleanup
             os.remove(local_path)
+            print "Removed file from local path"
             s3_url = key.generate_url(expires_in=0, query_auth=False,
                      force_http=True)
             print s3_url
@@ -146,6 +162,7 @@ def upload():
                 'video_url': s3_url,
             }
             resp = _make_api_request('post', '/videos/', data=json.dumps(data))
+            print "Submitted the video"
             print resp.status_code
             """
             if resp.status_code != 201:
@@ -153,7 +170,7 @@ def upload():
                 return redirect(url_for('index'))
             """
             video_data = json.loads(resp.content)
-            #print video_data
+            print video_data
             if not video_data.has_key('id'):
                 flash('Video already exists...', 'danger')
                 return redirect(url_for('index'))
@@ -169,6 +186,7 @@ def upload():
                 }
             resp = _make_api_request('PUT', '/videos/{}/'.format(video_data.get('id')),
                     data=json.dumps(data))
+	    print "Moved video to the selected team and project"
     return redirect(url_for('index'))
 
 @app.route('/logout/')
